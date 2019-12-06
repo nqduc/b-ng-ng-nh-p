5652ed0b2245e2f628236c8ee35813b6d8ae35f9
nguyenquangduc <ducnguyenquang05@gmail.com>
2019-12-06 12:33:10 +0900

    3file

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+//    include('config.php');
+   session_start();
+   
+   if(!isset($_SESSION['user']) && $_SESSION['pass']){
+    // var_dump($_SESSION);
+    echo "Thành Công";
+
+//    $user_check = $_SESSION['login_user'];
+   
+//    $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+   
+//    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+//    $login_session = $row['username'];
+   
+//    if(!isset($_SESSION['user']) && $_SESSION['pass']){
+//        var_dump($_SESSION);
+//       header("location:login.php");
+//       die();
+   }
+?>
---
+++ login.php
@@ -0,0 +1,20 @@
+<?php 
+    session_start();
+    $username = $_POST['name'];
+    
+    $pass = $_POST['pass'];
+    
+    if ($username == "anhduc" and $pass == "123456"){
+        echo "Thành Công";
+        $_SESSION [ 'use'] = $username;
+        $_SESSION['pass'] = $pass;
+        var_dump($_SESSION);
+        die();
+    } else {
+        echo "Không Thành Công";
+    }
+
+    // if(session_destroy(){
+    header ("location: login.php");
+    // };
+?>
